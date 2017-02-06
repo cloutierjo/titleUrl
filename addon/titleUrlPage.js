fe08b10c002d8e8f2bff1480ee602b7750834271
Jonatan Cloutier <cloutier.jo@gmail.com>
2017-02-05 20:10:41 -0500

    remove debug log

--- addon/titleUrlPage.js
+++ addon/titleUrlPage.js
@@ -1,11 +1,9 @@
 // window.addEventListener('load', updateUrl, false);
 browser.runtime.onMessage.addListener(updateUrl)
 updateUrl()
-console.log("page script");
 
 function updateUrl() {
     var title = getTitle();
-    console.log("yup",title);
     if(title){
        document.title = title;
     }
