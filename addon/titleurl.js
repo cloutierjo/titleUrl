51088cf717860646a9daeccdd0e6b2ac294d53ce
Offerel <14041522+Offerel@users.noreply.github.com>
2019-03-22 11:51:24 +0100

    Optional full URL
    * added option to display full URL
    * added and fixed patch from @gagarine to "Only react when the URL is changed, not all events"

--- addon/titleurl.js
+++ addon/titleurl.js
@@ -1,12 +1,12 @@
-
 function updateUrl(tabId, changeInfo, tabInfo) {
-    browser.tabs.sendMessage(tabId, {action:"updateUrl"});
+	if(typeof changeInfo !== 'undefined' && typeof changeInfo.url !== 'undefined') {
+        browser.tabs.sendMessage(tabId, {action:"updateUrl"});
+    }
 }
 
 function updateTab(tab, changeInfo, tabInfo) {
     updateUrl(tab.tabId, changeInfo, tabInfo)
 }
-
 
 browser.tabs.onUpdated.addListener(updateUrl);
 browser.tabs.onActivated.addListener(updateTab);
